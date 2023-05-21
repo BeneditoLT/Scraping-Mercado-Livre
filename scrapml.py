8b79fb1a054c5422550b968957d1464555a3a3cc
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-05-21 18:38:15 -0300

    Add files via upload

--- scrapml.py
+++ scrapml.py
@@ -11,43 +11,34 @@ def scrap():
                 site = requests.get(url2, headers=headers)
                 soup = BeautifulSoup(site.content, 'html.parser') 
                 titulo = soup.find('h1', class_='ui-pdp-title').get_text()
-                valor = soup.find('span', class_='price-tag-fraction').get_text()  
+               ## valor = soup.find('span', class_='price-tag-fraction').get_text()  
                 vendas = soup.find('span', class_='ui-pdp-subtitle').get_text()
                 nun_vendas = vendas[7:-9]
                 url2 = str(url2)
-                linha = titulo+','+ valor+','+nun_vendas
+                linha = titulo+','+','+nun_vendas
                
                 print(linha)
                 f.write(linha)
                 f.write('\n')
                 
 
-url = input('Incira uma lista :')
+url = 'https://lista.mercadolivre.com.br/_CustId_77445148'
 
 headers = {
         'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
 
 site = requests.get(url, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')  
-pagina= soup.find('li', class_='andes-pagination__page-count').get_text()  
-pagina = int(pagina[3:])
-link = soup.find_all('a', class_='ui-search-result__content ui-search-link')
+
+link = soup.find_all( 'a',class_='ui-search-item__group__element shops__items-group-details ui-search-link') 
+
 
 item = 0
 j = len(link) 
 with open ('stm.csv', 'a', newline ='', encoding= 'UTF-8') as f:
          
         scrap()
-        while pagina > 1:
-                urlp = soup.find('a', class_='andes-pagination__link ui-search-link').get('href')
-                site = requests.get(urlp, headers=headers)
-                soup = BeautifulSoup(site.content, 'html.parser')  
-                link = soup.find_all('a', class_='ui-search-result__content ui-search-link')
-                j = len(link) 
-                scrap()
-                pagina = pagina -1 
-                
 
-        
-        
-               
+
+         
+     
